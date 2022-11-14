3f11dbb1d712aa2ed6a3cd32145b8161999d483f
john0212 <df850044@gmail.com>
2022-11-14 16:07:55 +0800

    增加 update Task 頁面及把更新資料存進資料庫中

--- springboot-todolist/src/main/java/com/john/springboottodolist/controller/TaskController.java
+++ springboot-todolist/src/main/java/com/john/springboottodolist/controller/TaskController.java
@@ -5,10 +5,7 @@ import com.john.springboottodolist.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,34 +22,45 @@ public class TaskController {
 
     // 顯示全部
     @GetMapping("/list")
-    public String listTasks(Model theModel){
+    public String listTasks(Model theModel) {
 
         // 從資料庫取出資料
         List<Task> taskList = taskService.findAll();
 
         // 添加到 spring model
-        theModel.addAttribute("tasks",taskList);
+        theModel.addAttribute("tasks", taskList);
 
         return "tasks/list-tasks";
     }
 
     @GetMapping("/showFormForAdd")
-    public String showFormForAdd(Model theModel){
+    public String showFormForAdd(Model theModel) {
 
         Task task = new Task();
 
-        theModel.addAttribute("task",task);
+        theModel.addAttribute("task", task);
 
         return "tasks/task-form";
     }
 
     @PostMapping("/save")
     // 這裡的 @ModelAttribute的 task 要跟 task-form 裡的  th:object="${task} 這裡一樣
-    public String saveTask(@ModelAttribute("task") Task theTask){
+    public String saveTask(@ModelAttribute("task") Task theTask) {
 
         // save the task
         taskService.save(theTask);
 
         return "redirect:/tasks/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("taskId") int theId,
+                                    Model theModel) {
+        // 從 service 取得 task
+        Task theTask = taskService.findById(theId);
+
+        theModel.addAttribute("task",theTask);
+
+        return "tasks/task-form";
     }
 }
